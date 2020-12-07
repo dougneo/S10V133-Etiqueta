92eb61e9d29dd977dd91d24f508afb68ef64f2b0
Douglas <doug.neo@gmail.com>
2020-12-07 13:45:49 -0300

    Classe Used Product finalizada.

--- S10V133 Etiqueta/Enteties/UsedProduct.cs
+++ S10V133 Etiqueta/Enteties/UsedProduct.cs
@@ -1,7 +1,25 @@
-﻿namespace S10V133_Etiqueta.Enteties
+﻿using System;
+using System.Globalization;
+
+namespace S10V133_Etiqueta.Enteties
 {
     class UsedProduct :Product
     {
+        public DateTime ManufactureDate { get; set; }
 
+        public UsedProduct()
+        {
+        }
+
+        public UsedProduct(string name, double price, DateTime manufactureDate) 
+            :base(name,price)
+        {
+            ManufactureDate = manufactureDate;
+        }
+
+        public override string PriceTag()
+        {
+            return Name + "(Used) $ " + Price.ToString("F2", CultureInfo.InvariantCulture)+" (Manufacture date: "+ManufactureDate.ToString("dd/MM/yyyy)");
+        }
     }
 }
