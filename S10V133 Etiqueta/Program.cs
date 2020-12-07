124fa7f63e1693c4628de0d4627d79d36d753079
Douglas <doug.neo@gmail.com>
2020-12-07 14:24:20 -0300

    Condigo fonte principal finalizado.

--- S10V133 Etiqueta/Program.cs
+++ S10V133 Etiqueta/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using S10V133_Etiqueta.Enteties;
+
 
 namespace S10V133_Etiqueta
 {
@@ -6,7 +9,46 @@ namespace S10V133_Etiqueta
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Enter the number of products: ");
+            int n = int.Parse(Console.ReadLine());
+
+            List<Product> list = new List<Product>();
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Product #{i} data:");
+
+                Console.Write("Common, used or imported (c/u/i)? ");
+                string situation = Console.ReadLine();
+
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+
+                Console.Write("Price: ");
+                double price = double.Parse(Console.ReadLine());
+
+                if (situation == "i")
+                {
+                    Console.Write("Customs fee: ");
+                    double tax = double.Parse(Console.ReadLine());
+                    list.Add(new ImportedProduct(name, price, tax));
+                }
+                else if (situation == "u")
+                {
+                    Console.Write("Manufacture date (DD/MM/YYYY): ");
+                    DateTime manufacture = DateTime.Parse(Console.ReadLine());
+                    list.Add(new UsedProduct(name, price, manufacture));
+                }
+
+                list.Add(new Product(name, price));
+            }
+            Console.WriteLine();
+            Console.WriteLine("PRICE TAGS:");
+
+            foreach (Product prod in list)
+            {
+                Console.WriteLine(prod);
+            }
         }
     }
 }
