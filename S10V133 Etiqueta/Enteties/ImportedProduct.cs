19c2988655bec6b5577a00e08e92aea008e3df9c
Douglas <doug.neo@gmail.com>
2020-12-07 13:53:02 -0300

    Classe Imported Product finalizada.

--- S10V133 Etiqueta/Enteties/ImportedProduct.cs
+++ S10V133 Etiqueta/Enteties/ImportedProduct.cs
@@ -1,7 +1,29 @@
-﻿namespace S10V133_Etiqueta.Enteties
+﻿using System.Globalization;
+
+namespace S10V133_Etiqueta.Enteties
 {
     class ImportedProduct:Product
     {
+        public double CustomsFee { get; set; }
 
+        public ImportedProduct()
+        {
+        }
+
+        public ImportedProduct(string name, double price,double customsFee)
+            :base (name, price)
+        {
+            CustomsFee = customsFee;
+        }
+
+        public override string PriceTag()
+        {
+            return base.PriceTag()+ "Customs Fee: $ "+  CustomsFee;
+        }
+
+        public double TotalPrice()
+        {
+            return Price + CustomsFee;
+        }
     }
 }
